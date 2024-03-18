cb5be5d40efef4de279fe41b72772177e8bcdd04
zacharyuhc <zachary.hue21@gmail.com>
2024-03-18 19:32:36 +0100

    chore: New View Paste style

--- src/components/ViewPaste.tsx
+++ src/components/ViewPaste.tsx
@@ -2,10 +2,9 @@ import { useState, useEffect } from "react";
 
 // Components
 import { Input } from "@/components/ui/input";
-import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
-import { Separator } from "@/components/ui/separator";
 import { Toaster } from "@/components/ui/sonner";
 import {
   Dialog,
@@ -31,7 +30,7 @@ import AceEditor from "react-ace";
 import { supabase } from "../supabase/supabaseClient";
 
 // Analytics
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 function ViewPaste() {
   const [pastes, setPastes] = useState<any[] | null>(null);
@@ -83,147 +82,158 @@ function ViewPaste() {
   };
   return (
     <>
-    <Analytics/>
+      <Analytics />
       <Toaster
         toastOptions={{
           className: "text-left z-50",
         }}
       />
-      <Card className="w-[350px]" style={{ height: "-webkit-fill-available" }}>
-        <CardContent>
-          <div className="grid w-full items-center gap-4 pt-6">
-            <div className="flex flex-col space-y-1.5">
-              <Label>Title</Label>
-              <Input
-                id="title"
-                value={
-                  pastes
-                    ? pastes
-                        .map((paste: { title: string }) => paste.title)
-                        .join("\n")
-                    : ""
-                }
-                readOnly
-              />
+      <div>
+        <Card
+          className="w-[350px] mb-4"
+          style={{ height: "-webkit-fill-available" }}
+        >
+          <CardContent>
+            <div className="grid w-full items-center gap-4 pt-6">
+              <div className="flex flex-col space-y-1.5">
+                <Input
+                  id="title"
+                  value={
+                    pastes
+                      ? pastes
+                          .map((paste: { title: string }) => paste.title)
+                          .join("\n")
+                      : ""
+                  }
+                  readOnly
+                />
+              </div>
             </div>
-          </div>
-          <div className="grid w-full items-center gap-4 pt-6">
-            <Dialog>
-              <DialogTrigger>
-                <Button className="w-full">More Info</Button>
-              </DialogTrigger>
-              <DialogContent onOpenAutoFocus={(e) => e.preventDefault()}>
-                <DialogHeader>
-                  <DialogTitle>Paste Info</DialogTitle>
-                  <DialogDescription>
-                    You can find all the information on the current paste below.
-                  </DialogDescription>
-                </DialogHeader>
-                <div className="flex flex-col space-y-1.5">
-                  <Label>ID</Label>
-                  <Input
-                    id="title"
-                    value={
-                      pastes
-                        ? pastes
-                            .map((paste: { id: string }) => paste.id)
-                            .join("\n")
-                        : ""
-                    }
-                    readOnly
-                  />
-                </div>
-                <div className="flex flex-col space-y-1.5">
-                  <Label>Syntax</Label>
-                  <Input
-                    className="capitalize"
-                    id="title"
-                    value={
-                      pastes
-                        ? pastes
-                            .map((paste: { syntax: string }) =>
-                              paste.syntax === "no" ? "No Syntax" : paste.syntax
-                            )
-                            .join("\n")
-                        : ""
-                    }
-                    readOnly
-                  />
-                </div>
-                <div className="flex flex-col space-y-1.5">
-                  <Label>Visibility</Label>
-                  <Input
-                    className="capitalize"
-                    id="title"
-                    value={
-                      pastes
-                        ? pastes
-                            .map(
-                              (paste: { visibility: string }) =>
-                                paste.visibility
-                            )
-                            .join("\n")
-                        : ""
-                    }
-                    readOnly
-                  />
-                </div>
-                <div className="flex flex-col space-y-1.5">
-                  <Label>Time (UTC+1)</Label>
-                  <Input
-                    className="capitalize"
-                    id="title"
-                    value={
-                      pastes
-                        ? pastes
-                            .map(
-                              (paste: { timestamp: string }) => paste.timestamp
-                            )
-                            .join("\n")
-                        : ""
-                    }
-                    readOnly
-                  />
-                </div>
-                <div className="flex flex-col space-y-1.5">
-                  <Label>Link</Label>
-                  <div className="flex gap-2">
+            <div className="grid w-full items-center gap-4 pt-6">
+              <Dialog>
+                <DialogTrigger>
+                  <Button className="w-full">More Info</Button>
+                </DialogTrigger>
+                <DialogContent onOpenAutoFocus={(e) => e.preventDefault()}>
+                  <DialogHeader>
+                    <DialogTitle>Paste Info</DialogTitle>
+                    <DialogDescription>
+                      You can find all the information on the current paste
+                      below.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="flex flex-col space-y-1.5">
+                    <Label>ID</Label>
                     <Input
                       id="title"
                       value={
-                        "https://probin.xyz/" +
-                        (pastes
+                        pastes
                           ? pastes
                               .map((paste: { id: string }) => paste.id)
                               .join("\n")
-                          : "")
+                          : ""
                       }
                       readOnly
                     />
-                    <Button
-                      onClick={handleCopyClick}
-                      size="sm"
-                      className="px-3"
-                    >
-                      <span className="sr-only">Copy</span>
-                      <FontAwesomeIcon className="h-4 w-4" icon={faCopy} />
-                    </Button>
                   </div>
-                </div>
-              </DialogContent>
-            </Dialog>
-          </div>
-          <Separator className="mt-3" />
-        </CardContent>
-        <CardFooter className="flex justify-between gap-2">
-          <Button variant={"secondary"} className="w-full">
-            <FontAwesomeIcon icon={faCopy} />
-          </Button>
-          <Button variant={"secondary"} className="w-full">
-            <FontAwesomeIcon icon={faDownload} />
-          </Button>
-        </CardFooter>
-      </Card>
+                  <div className="flex flex-col space-y-1.5">
+                    <Label>Syntax</Label>
+                    <Input
+                      className="capitalize"
+                      id="title"
+                      value={
+                        pastes
+                          ? pastes
+                              .map((paste: { syntax: string }) =>
+                                paste.syntax === "no"
+                                  ? "No Syntax"
+                                  : paste.syntax
+                              )
+                              .join("\n")
+                          : ""
+                      }
+                      readOnly
+                    />
+                  </div>
+                  <div className="flex flex-col space-y-1.5">
+                    <Label>Visibility</Label>
+                    <Input
+                      className="capitalize"
+                      id="title"
+                      value={
+                        pastes
+                          ? pastes
+                              .map(
+                                (paste: { visibility: string }) =>
+                                  paste.visibility
+                              )
+                              .join("\n")
+                          : ""
+                      }
+                      readOnly
+                    />
+                  </div>
+                  <div className="flex flex-col space-y-1.5">
+                    <Label>Time (UTC+1)</Label>
+                    <Input
+                      className="capitalize"
+                      id="title"
+                      value={
+                        pastes
+                          ? pastes
+                              .map(
+                                (paste: { timestamp: string }) =>
+                                  paste.timestamp
+                              )
+                              .join("\n")
+                          : ""
+                      }
+                      readOnly
+                    />
+                  </div>
+                  <div className="flex flex-col space-y-1.5">
+                    <Label>Link</Label>
+                    <div className="flex gap-2">
+                      <Input
+                        id="title"
+                        value={
+                          "https://probin.xyz/" +
+                          (pastes
+                            ? pastes
+                                .map((paste: { id: string }) => paste.id)
+                                .join("\n")
+                            : "")
+                        }
+                        readOnly
+                      />
+                      <Button
+                        onClick={handleCopyClick}
+                        size="sm"
+                        className="px-3"
+                      >
+                        <span className="sr-only">Copy</span>
+                        <FontAwesomeIcon className="h-4 w-4" icon={faCopy} />
+                      </Button>
+                    </div>
+                  </div>
+                </DialogContent>
+              </Dialog>
+            </div>
+          </CardContent>
+        </Card>
+        <Card className="pt-6">
+          <CardContent>
+            <div className="flex gap-4">
+              <Button variant={"outline"} className="w-full">
+                <FontAwesomeIcon icon={faCopy} />
+              </Button>
+              <Button variant={"outline"} className="w-full">
+                <FontAwesomeIcon icon={faDownload} />
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
       <Card className="ml-6 w-full">
         <CardContent className="p-0">
           <AceEditor
